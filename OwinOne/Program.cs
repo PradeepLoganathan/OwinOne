ff6088262ab32bc8b86b6627f82a474e6df70923
Pradeep Loganathan <pradeep@pradeeploganathan.com>
2017-03-11 19:29:02 +0530

    Add project files.

---
+++ OwinOne/Program.cs
@@ -0,0 +1,33 @@
+﻿using Owin;
+using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwinOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:8089"))
+            {
+                Console.WriteLine("Server started...");
+                Console.Read();
+            }
+        }
+    }
+
+    class Startup
+    {
+        public void Configuration(IAppBuilder App)
+        {
+            //App.Run( ctx =>
+            //{
+            //    return ctx.Response.WriteAsync("Hello World");
+            //});
+        }
+    }
+}
